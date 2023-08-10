511b3d77d99b0321979034148b27f566a5de07cf
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-08-10 00:24:30 -0300

    Lista de opções adicionada no header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import './App.css'
 import Logo from './components/Logo/Logo'
+import iconUser from './images/perfil.svg'
+import iconBag from './images/sacola.svg'
 
 export default function App() {
+  
+  const textOptions = ['CATEGORIAS', 'FAVORITOS', 'MINHA ESTANTE']
+  const icons = [iconUser, iconBag]
+
   return (
     <div className='App'>
       <header className='App-header'>
         <Logo />
+        <ul className='options'>
+          { textOptions.map ( (text) => (
+              <li className='option'><p>{text}</p></li>
+          ) ) }
+        </ul>
+        <ul className='icons'>
+            { icons.map( (icon) => (
+              <li className='icon'><img src={icon}></img></li>
+            )) }
+        </ul>
       </header>
     </div>
   )
