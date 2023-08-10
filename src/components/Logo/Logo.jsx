595bc7ad6d080a2c0f4c7a813d6351ace6ed47ad
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-08-09 23:00:19 -0300

    Pastas iniciais, imagens e primeiro componente adicionados

---
+++ src/components/Logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './Logo.css'
+// import logo from 'C:/src/magoo-books/src/images/logo.svg'
+import logo from '../../images/logo.svg'
+
+export default function Logo() {
+    return (
+        <div className='logo'>
+            <img src={logo} alt='logo' />
+            <p><strong>Magoo</strong>Books</p>
+        </div>
+    )
+}
