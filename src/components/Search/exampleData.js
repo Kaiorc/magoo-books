ce08810262a4fa67636a4c8baabaf7b6597686c2
Kaio Rodrigues <kaio.cunha@aluno.uece.br>
2023-08-21 22:25:47 -0300

    Arquivo de exemplos de livros adicionado. Exibição de resultados de pesquisa de livros adicionada

---
+++ src/components/Search/exampleData.js
@@ -0,0 +1,14 @@
+import cover from '../../assets/book-cover.png'
+
+export const books = [
+    { name: "Harry Potter e a Pedra Filosofal", id: 0, src: cover },
+    { name: "O Senhor dos Anéis: A Sociedade do Anel", id: 1, src: cover },
+    { name: "1984", id: 2, src: cover },
+    { name: "Cem Anos de Solidão", id: 3, src: cover },
+    { name: "Dom Quixote", id: 4, src: cover },
+    { name: "O Pequeno Príncipe", id: 5, src: cover },
+    { name: "Em Busca do Tempo Perdido", id: 6, src: cover },
+    { name: "Dom Casmurro", id: 7, src: cover },
+    { name: "O Grande Gatsby", id: 8, src: cover },
+    { name: "Matar um Rouxinol", id: 9, src: cover }  
+]
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,7 +1,9 @@
 import styled from 'styled-components'
 import Input from '../Input'
 import React from 'react'
+import { books } from './exampleData.js'
 
+// Bloco de estilização dos styled components
 const SearchContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
     color: #FFF;
@@ -24,8 +26,33 @@ const Subtitle = styled.h3`
     margin-bottom: 40px;
 `
 
+const Result = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+    p {
+        width: 200px;
+    }
+    img {
+        width: 100px;
+    }
+    &:hover {
+        border: 1px solid white;
+    }
+`
+
 export default function Search() {
-    const [searchText, setSearchText] = React.useState('')
+    /* State que salva o array de livros pesquisados */ 
+    const [booksSearch, setBooksSearch] = React.useState([])
+
+    const booksShowcases = booksSearch.map( book => (
+        <Result>
+            <img src={book.src}/>
+            <p>{book.name}</p>
+        </Result>
+    )) 
 
     return(
         <SearchContainer>
@@ -33,9 +60,17 @@ export default function Search() {
             <Subtitle>Pesquise na estante</Subtitle>
             <Input 
                 placeholder="Escreva aqui sua pesquisa"
-                onBlur={event => setSearchText(event.target.value)}
+                onBlur={event => {
+                    const enteredValue = event.target.value
+                    if(enteredValue != ""){
+                        const searchResult = books.filter(book => book.name.toLowerCase().includes(enteredValue.toLowerCase()))
+                        setBooksSearch(searchResult)
+                    } else {
+                        setBooksSearch([])
+                    }
+                }}
             />
-            <p>{searchText}</p>
+            { booksShowcases }
         </SearchContainer>
     )
 }
